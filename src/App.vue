7956be034a71ae919c8b698cdf9a92e09c71f832
manageritcast <manageritcast@126.com>
2017-10-19 11:14:42 +0800

    03.完成了显示和隐藏返回按钮及底部TabBar
    完成了显示和隐藏返回按钮及底部TabBar

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@
     <!-- 1.0 头部 -->
     <mt-header fixed title="Vue_CMS"></mt-header>
 
+    <div v-show="isShowBack" @click="goBack" class="backStyle">&lt;返回</div>
+
     <!-- 2.0 中间内容 路由 -->
     <router-view class="centerContent"></router-view>
 
     <!-- 3.0 底部TabBar -->
-    <mt-tabbar fixed>
+    <mt-tabbar :class="isHideTabBar ? 'hideTabBarStyle' : '' " fixed>
       <mt-tab-item>
         <router-link to="/home">
           <img src="http://img08.jiuxian.com/bill/2016/0224/cccd8df26a754c139de800406af82178.png">
@@ -46,6 +48,55 @@
   .centerContent {
     margin-top: 40px;
   }
+
+  /* 返回按钮样式 */
+  .backStyle{
+    position: fixed;
+    left: 8px;
+    top:10px;
+    font-size: 14px;
+    font-weight: 700;
+    color: white;
+    z-index: 2;
+  }
+
+  /* 隐藏底部TabBar */
+  .hideTabBarStyle{
+    display: none;
+  }
 </style>
+
+<script>
+  export default{
+    data(){
+      return {
+        isShowBack:false,
+        isHideTabBar:false
+      }
+    },
+    created(){
+      this.isShowOrHide(this.$route.path)
+    },
+    methods:{
+      goBack(){
+        this.$router.go(-1) //底层等同于 history.back()
+      },
+      isShowOrHide(path){//根据路径决定显示还是影藏返回按钮和底部TabBar
+        if(path!='/home'){
+          this.isShowBack = true
+          this.isHideTabBar = true
+        }else{//首页
+          this.isShowBack = false
+          this.isHideTabBar = false
+        }
+      }
+    },
+    watch:{
+      $route:function(newValue, oldValue){
+        this.isShowOrHide(newValue.path)
+      }
+    }
+  }
+</script>
 
 
