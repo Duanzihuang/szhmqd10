e12e3552e2d37cb39f090760bb8f21e449daaf89
manageritcast <manageritcast@126.com>
2017-10-21 11:59:30 +0800

    12.完成了商品列表的功能

---
+++ src/components/goods/goodslist.vue
@@ -0,0 +1,109 @@
+<template>
+   <div class="mui-content" style="background-color:#fff">
+        <ul class="mui-table-view mui-grid-view">
+            <li v-for="item in goodsList" :key="item.id" class="mui-table-view-cell mui-media mui-col-xs-6">
+                <div class="goodsContentStyle">
+                    <a href="#">
+                        <img class="mui-media-object" :src="item.img_url">
+                        <div class="mui-media-body">
+                            <!-- 标题 -->
+                            <p class="titleStyle">{{item.title}}</p>
+                            <!-- 价格 -->
+                            <p class="priceStyle">
+                                <span>{{item.sell_price}}</span>&nbsp;&nbsp;
+                                <s>{{item.market_price}}</s>
+                            </p>
+                            <!-- 提示信息 -->
+                            <p class="tipStyle">
+                                <span>热卖中</span>
+                                <span>剩余{{item.stock_quantity}}件</span>
+                            </p>
+                        </div>
+                    </a>
+                </div>
+            </li>
+        </ul>    
+    </div>
+</template>
+   
+<style scoped>
+   .mui-content ul{
+       margin: 0px;
+       padding: 0px;
+   }
+
+   .mui-table-view.mui-grid-view .mui-table-view-cell{
+       margin: 0px;
+       padding: 5px 6px;
+   }
+
+   .mui-content .mui-grid-view .goodsContentStyle{
+       border: 1px solid rgba(92,92,92,0.3);
+       border-radius: 5px;
+       box-shadow: 0px 0px 5px rgba(92,92,92,0.3);
+   }
+
+   img{
+       padding: 5px;
+   }
+
+   .mui-content .mui-grid-view .goodsContentStyle .mui-media-body{
+       height: 100px;
+       padding: 5px;
+   }
+
+   .titleStyle{
+       color: black;
+       font-size: 12px;
+       text-align: left;
+       overflow:hidden; 
+       text-overflow:ellipsis;
+       display:-webkit-box;
+       -webkit-box-orient:vertical;
+       -webkit-line-clamp:2;
+   }
+
+   .priceStyle{
+       height: 35px;
+       background-color: #f0f0f0;
+       text-align: left;
+       line-height: 35px;
+   }
+
+   .priceStyle span{
+       color: red;
+       font-size: 16px;
+   }
+
+   .tipStyle{
+       height: 25px;
+       background-color: #f0f0f0;
+       display: flex;
+       justify-content: space-between;
+       align-items: center;
+   }
+</style>
+   
+<script>
+   import common from '../../common/common.js'
+   
+   export default {
+       data() {
+           return {
+               goodsList:[]
+           }
+       },
+       created() {
+           this.getGoodsListData()
+       },
+       methods: {
+           getGoodsListData(){
+               const url = `${common.apihost}api/getgoods`
+
+               this.$http.get(url).then(response=>{
+                   this.goodsList = response.body.message
+               })
+           }
+       }
+   }
+</script>
--- src/main.js
+++ src/main.js
@@ -47,6 +47,7 @@ import newslist from './components/news/newslist.vue'
 import newsinfo from './components/news/newsinfo.vue'
 import photolist from './components/photo/photolist.vue'
 import photoinfo from './components/photo/photoinfo.vue'
+import goodslist from './components/goods/goodslist.vue'
 
 //创建路由对象，设置路由规则
 const router = new VueRouter({
@@ -59,7 +60,8 @@ const router = new VueRouter({
         {path:'/news/newslist',component:newslist},
         {path:'/news/newsinfo/:newsId',component:newsinfo},
         {path:'/photo/photolist',component:photolist},
-        {path:'/photo/photoinfo/:photoId',component:photoinfo}
+        {path:'/photo/photoinfo/:photoId',component:photoinfo},
+        {path:'/goods/goodslist',component:goodslist}
     ]
 })
 
